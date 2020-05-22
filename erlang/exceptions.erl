8352dea9f0d6162a6f59eeec8136681dee72a727
XandeCoding <af.santos1998@gmail.com>
2020-05-21 22:06:52 -0300

    Exceptions exercise

---
+++ erlang/exceptions.erl
@@ -0,0 +1,20 @@
+%% Programa que testa varios tipos de excecoes
+
+-module(exceptions).
+-compile(export_all).
+
+sword(1) -> throw(slice);
+sword(2) -> erlang:error(cut_arm);
+sword(3) -> exit(cut_leg);
+sword(4) -> throw(punch);
+sword(5) -> exit(cross_bridge).
+
+black_knight(Attack) when is_function(Attack, 0) ->
+    try Attack() of
+        _ -> "None shall pass."
+    catch
+        throw: slice -> "It is but a scratch.";
+        error: cut_arm -> "I've had worse.";
+        exit: cut_leg -> "Come on your bastart";
+        _:_ -> "Just a flesh wound."
+    end.
