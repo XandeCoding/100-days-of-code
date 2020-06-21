ffa842e18d1281d74d5eb21314fce2721217a692
XandeCoding <af.santos1998@gmail.com>
2020-06-20 21:32:19 -0300

    github issues fetch application

---
+++ elixir/issues/mix.exs
@@ -0,0 +1,35 @@
+defmodule Issues.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :issues,
+      escript: escript_config(),
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      { :httpoison, "~> 1.0.0" },
+      { :poison, "~> 4.0.0" },
+      { :ex_doc, "~> 0.22.1" },
+      { :earmark, "~> 1.4.5"}
+    ]
+  end
+  defp escript_config do
+    [
+      main_module: Issues.CLI
+    ]
+  end
+end
