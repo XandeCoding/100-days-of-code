2ad5331927ed5ed96eceb815e932b3e05d5623cf
XandeCoding <af.santos1998@gmail.com>
2020-06-25 09:04:35 -0300

    update

--- elixir/issues/mix.exs
+++ elixir/issues/mix.exs
@@ -6,9 +6,18 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config(),
       version: "0.1.0",
+      name: "Issues",
       elixir: "~> 1.10",
       source_url: "https://github.com/XandeCoding/100-days-of-code",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
+      test_coverage: [ tool: ExCoveralls ],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -25,7 +34,8 @@ defmodule Issues.MixProject do
       { :httpoison, "~> 1.0.0" },
       { :poison, "~> 4.0.0" },
       { :ex_doc, "~> 0.22.1" },
-      { :earmark, "~> 1.4.5"}
+      { :earmark, "~> 1.4.5" },
+      { :excoveralls, "~> 0.13.0", only: :test }
     ]
   end
   defp escript_config do
